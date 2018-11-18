79bbcd137c5270eddb48f5af4d4673a5b7dca7f8
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2018-11-18 22:55:00 +0100

    Add pug and few new routes.

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
+// Load views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
+// Home route
+app.get('/', function (req, res) {
+  res.render('index', {
+    variable: 'I am some variable'
+  })
+});
+
+// Add Article
+app.get('/add', function (req, res) {
+  res.render('add_article', {
+    title: 'Add Article'
+  })
+});
+
+// Start server
 app.listen(3000, function() {
   console.log('Server started on port 3000');
-})
+});
