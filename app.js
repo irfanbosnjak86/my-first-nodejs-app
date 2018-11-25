da6752d230c7761b3c728bf010c6248736d3e888
Irfan Bosnjak <irfan.bosnjak86@gmail.com>
2018-11-25 22:29:54 +0100

    Add Pug, Body Parser to package.json. Add connection to mongodb. Add create article endpoint.

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 const express = require('express');
 const path = require('path')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
+// Setup database
+mongoose.connect('mongodb://localhost/my-first-node-js-app');
+
+let db = mongoose.connection;
+
+// Check db connect
+db.once('open', function(){
+  console.log('Connected to mongodb');
+});
+
+// Check for db errors
+db.on('error', function(error){
+  console.log(error);
+});
+
+// Init App
 const app = express();
+
+// Body parser middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Bring in models
+let Article = require('./models/article')
 
 // Load views
 app.set('views', path.join(__dirname, 'views'));
@@ -9,16 +37,41 @@ app.set('view engine', 'pug');
 
 // Home route
 app.get('/', function (req, res) {
-  res.render('index', {
-    variable: 'I am some variable'
-  })
+  Article.find({}, function(error, articles){
+    if(error){
+      console.log(error)
+    } else {
+      res.render('index', {
+        title: 'Articles',
+        articles: articles
+      });
+    }
+  });
 });
 
 // Add Article
-app.get('/add', function (req, res) {
+app.get('/articles/add', function (req, res) {
   res.render('add_article', {
     title: 'Add Article'
   })
+});
+
+// Add submit POST to articles
+app.post('/articles/add', function (req, res) {
+  let article = new Article();
+
+  article.title = req.body.title;
+  article.body = req.body.body;
+  article.author = req.body.author;
+
+  article.save(function(err){
+    if(err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  });
 });
 
 // Start server
